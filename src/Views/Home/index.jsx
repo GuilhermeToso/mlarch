bed32fb7c09c645054363ddc95ed750948a164fd
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-24 18:09:29 -0300

    Created Home page component

---
+++ src/Views/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HomeBody } from "../../Components/Body";
+
+function Home() {
+    return (
+        <HomeBody>
+            <h1>Hello World!!</h1>
+        </HomeBody>
+    )
+}
+
+export default Home
