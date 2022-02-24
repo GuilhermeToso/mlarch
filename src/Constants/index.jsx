ccbbf4ea8142b0350105ad2d7873b7de99b06d7e
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-24 18:08:55 -0300

    Created BACKGROUND and BREAKPOINTS constants

---
+++ src/Constants/index.jsx
@@ -0,0 +1,10 @@
+import BACKGROUND from '../Assets/Images/bg.jpg'
+export const BREAKPOINTS = {
+    xsm:"330px",
+    sm:"540px",
+    md:"768px",
+    lg:"1024px",
+    xlg:"1440px"
+}
+
+export { BACKGROUND }
