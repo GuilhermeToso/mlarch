c28b77b5b671a000b47c7319482981821dc9da93
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-24 18:07:55 -0300

    Created Body styles

---
+++ src/Components/Body/Body.styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { BACKGROUND } from "../../Constants";
+
+export const Body = styled.div`
+    position: absolute;    
+    width: 100%;
+    height: 100%;
+    display grid;
+`
+
+export const HomeBody = styled(Body)`
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr;
+    background: url(${BACKGROUND}) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+`
+
+export const AboutBody = styled(Body)`
+    grid-template-rows: 80px auto;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: "header header"
+                         "text draw"   
+`
