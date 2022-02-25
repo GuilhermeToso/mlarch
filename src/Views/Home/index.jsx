8369d5b66f9fda8d19ef2a41943346b8a7979e64
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-25 10:57:01 -0300

    Updated HomePage with HomeCard component

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { HomeBody } from "../../Components/Body";
-
+import { GetStarted, LearnMore } from "../../Components/Buttons";
+import { HomeCard } from "../../Components/Cards";
+import { Title } from "../../Components/Texts";
+import { Description, ButtonLabel } from "../../Components/Texts";
 function Home() {
     return (
         <HomeBody>
-            <h1>Hello World!!</h1>
+           <HomeCard>
+               <Title GridArea="home_card_title">Welcome to MLArch!</Title>
+               <Description GridArea="home_card_text">
+                   MLArch is a platform designed to find and build Deep Learning Architectures 
+                   in a friendly way. So let’s get started!
+                </Description>
+                <GetStarted GridArea="home_card_btn_1">
+                    <ButtonLabel Color="#000000">
+                        Get Started
+                    </ButtonLabel>
+                </GetStarted>
+                <LearnMore GridArea="home_card_btn_2">
+                    <ButtonLabel Color="#FFFFFF">
+                        Learn More
+                    </ButtonLabel>
+                </LearnMore>
+            </HomeCard>        
         </HomeBody>
     )
 }
