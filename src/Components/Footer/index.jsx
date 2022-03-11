2dae496ef14815c464e0adbe96945458cb5cf34e
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:27:41 -0300

    Created Footer component

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import { FooterStyle } from "./Footer.styles";
+
+const Footer = () => {
+    return (
+        <FooterStyle>
+            <h1 style={{color:'#000'}}>
+                This is the Footer
+            </h1>
+        </FooterStyle>
+    )
+}
+
+
+export default Footer
