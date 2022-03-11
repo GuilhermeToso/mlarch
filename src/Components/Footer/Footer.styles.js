ee1724609253438d35d076b490342559ed02881b
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:27:17 -0300

    Created FooterStyle styled component

---
+++ src/Components/Footer/Footer.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    background: #FAFAFA;
+    box-shadow: -2px -3px 8px rgba(0, 0, 0, 0.15);
+    border-radius: 2px
+`
