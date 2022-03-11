93af5b27b1986534eae32ef783661991162b0364
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:17:36 -0300

    Created Aside Component Style

---
+++ src/Components/Aside/Aside.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AsideStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    background: #FAFAFA;
+    box-shadow: 5px 0px 8px rgba(0, 0, 0, 0.15);
+`
