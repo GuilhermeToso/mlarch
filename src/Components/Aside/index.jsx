e303aa3d5347469a4d2903e3a78b3c0eea941ad8
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:18:03 -0300

    Created Aside Component

---
+++ src/Components/Aside/index.jsx
@@ -0,0 +1,15 @@
+import { AsideStyle } from "./Aside.styles";
+
+const Aside = () => {
+    return (
+        <AsideStyle>
+            <h1 style={{color:"#000"}}>
+                This is the Menu
+            </h1>
+        </AsideStyle>
+    )
+}
+
+export default Aside
+
+
