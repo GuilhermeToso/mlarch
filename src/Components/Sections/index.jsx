f02ca9d2c356dc651935baeee9a77be0e8f8da38
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:33:55 -0300

    Exported the Section' styled components

---
+++ src/Components/Sections/index.jsx
@@ -0,0 +1,8 @@
+import { SectionContainer, HomeSection, AppsSection, NoCodeSection } from "./Sections.styles";
+
+export {
+    SectionContainer,
+    HomeSection,
+    AppsSection,
+    NoCodeSection
+}
