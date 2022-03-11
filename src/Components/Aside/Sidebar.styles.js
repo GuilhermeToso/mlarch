e7224e28c7882272b7e81309f975e42da994aab1
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:19:47 -0300

    Created styled components for SideBar

---
+++ src/Components/Aside/Sidebar.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { UilTimes } from '@iconscout/react-unicons'
+
+export const SidebarContainer = styled.aside`
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+`
+
+export const Icon = styled.div``
+
+export const CloseIcon = styled.div``
