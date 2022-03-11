8cd3956a19b55ea44c8e5bf8baf94920a4e8072b
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:37:49 -0300

    Updated Texts' styled components

--- src/Components/Texts/Texts.styles.js
+++ src/Components/Texts/Texts.styles.js
@@ -1,26 +1,30 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
+    width: 100%;
+    height: 100%;
     font-family: Roboto;
     font-style: normal;
     font-weight: 800;
-    font-size: 48px;
+    font-size: min(48px,4vw);
     line-height: 56px;
     text-align: center;
-    padding: 32px 0;
-    color: #E7E7E7;
-    grid-area: ${props => props.GridArea}
+    color: #0F0F0F;
+    grid-area: ${props => props.GridArea};
 `
 
 
 export const Description = styled.h3`
+    width: 100%;
+    height: 100%;
+    position: relative;
     font-family: Roboto;
     font-style: normal;
     font-weight: 800;
-    font-size: 24px;
+    font-size: min(36px,3vw);
     line-height: 28px;
     text-align: center;
-    color: #E7E7E7;
+    color: #0f0f0f;
     grid-area: ${props => props.GridArea}
 `
 
@@ -32,4 +36,14 @@ export const ButtonLabel = styled.h2`
     line-height: 28px;
     text-align: center;
     color: ${props => props.Color};
+`
+export const LinkLabel = styled.h2`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    text-align: center;
 `
