721c11faf6f9a37b198c3024922b820ee3b50f94
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:35:37 -0300

    Created the Splitter styled component

---
+++ src/Components/Splits/Splits.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Split from "react-split";
+import '../../Styles/split.css'
+
+const Splitter = styled(Split)`
+    position: relative;    
+    width: 100%;
+    height: 100%;
+    display: flex;
+`
+
+export default Splitter
