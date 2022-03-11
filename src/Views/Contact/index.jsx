ed51873a8f738086c63cbd643d5c187cc67830a7
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:43:03 -0300

    Created Contact page component

---
+++ src/Views/Contact/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { HeadMainBody } from "../../Components/Body";
+import { HomeHeader } from "../../Components/Header";
+import { Main } from "../../Components/Main";
+
+const Contact = () => {
+    return(
+        <HeadMainBody>
+            <HomeHeader></HomeHeader>
+            <Main>
+                <h1>Contact Page</h1>
+            </Main>
+        </HeadMainBody>
+    )
+}
+
+export default Contact
