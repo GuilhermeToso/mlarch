14b3d27db6cda353169c9960c3db48fb6f9ad9e0
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-03-11 14:32:17 -0300

    Created Node' styled component

---
+++ src/Components/Nodes/Node.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Node = styled.div`
+    width: 100px;
+    height: 100px;
+    background:#fcfcfc;
+    border-radius:10px;
+    border: 1px solid #2d2d2d;
+    box-shadow: 3px 3px 3px #adadad;
+`
+
